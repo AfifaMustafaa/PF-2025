ba15ce87190ace094ffcf0350a33dd227fdfe468
AfifaMustafaa <afifa4820557@cloud.neduet.edu.pk>
2025-11-18 16:24:42 +0500

    Add files via upload

---
+++ practice .c/array practice.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int marks [5];
+	int i;
+	for (i=0 ;i<5 ;i++){
+	printf("enter marks of 4 students");
+	scanf("%d", &marks[10]);}
+	for (i=0 ;i<=3 ;i++){
+		if(marks[10]<35){
+			printf("%d\n" , i);
+		}
+	}
+	return 0;
+}
